749fe17505130b62dc81c596ee88fa15a91ef899
ItsJigzyy <albicus1994@gmail.com>
2021-05-21 22:48:03 +0100

    Initial bot commit for Heroku

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ1Mzk2ODE0Njg4OTQ0MTQ4.YKgXIw.7k2-XpcelMH38AlUSUeE68pbUNE',
+        prefix: '~',
+        activity: 'some sick beats',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
