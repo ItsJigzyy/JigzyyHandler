7f3836563a612a4e700e402139c842a1c0d34bb4
ItsJigzyy <albicus1994@gmail.com>
2021-05-22 01:31:08 +0100

    ff

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -10,15 +10,11 @@ module.exports = {
         const queue = client.player.getQueue(message);
 
         const StartEmbed = new MessageEmbed()
-
             .setTitle(`**Server queue - ${message.guild.name} ${client.emotes.queue}**`)   // \**
             .setColor("#ff331f")
-            .setDescription(`
-        Current : ${queue.playing.title}\n\n ` + (queue.tracks.map((track, i) => {
+            .setDescription(`Current : ${queue.playing.title}\n\n ` + (queue.tracks.map((track, i) => {
                 return `**#${i + 1}** - ${track.title} (requested by : ${track.requestedBy.username})`
             }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs` : `In the playlist **${queue.tracks.length}** song(s)`}`))
-
-
             .setThumbnail(`${message.guild.iconURL({ dynamic: true })}`)
             .setFooter(`Playing in ${message.member.voice.channel.name}`)
 
