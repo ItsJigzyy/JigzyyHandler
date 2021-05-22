e18d585e86a58a8e817fb403ac990ebf92619c14
ItsJigzyy <albicus1994@gmail.com>
2021-05-22 01:09:03 +0100

    ss

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,10 +1,27 @@
 module.exports = {
     name: 'queue',
-    aliases: [],
+    aliases: ['q'],
     category: 'Music',
     utilisation: '{prefix}queue',
 
     execute(client, message) {
+
+        const { Client, MessageEmbed } = require("discord.js");
+
+        const StartEmbed = new MessageEmbed()
+
+            .setTitle(`**Server queue - ${message.guild.name} ${client.emotes.queue} ${client.player.getQueue(message).loopMode ? '(looped)' : ''}**`)   // \**
+            .setColor("#ff331f")
+            .setDescription(`
+        Current : ${queue.playing.title}\n\n ` + (queue.tracks.map((track, i) => {
+                return `**#${i + 1}** - ${track.title} (requested by : ${track.requestedBy.username})`
+            }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs` : `In the playlist **${queue.tracks.length}** song(s)`}`))
+
+
+            .setThumbnail(`${message.guild.iconURL({ dynamic: true })}`)
+            .setFooter(`Playing in ${message.member.voice.channel.name}`)
+
+
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
@@ -13,8 +30,6 @@ module.exports = {
 
         if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
 
-        message.channel.send(`**Server queue - ${message.guild.name} ${client.emotes.queue} ${client.player.getQueue(message).loopMode ? '(looped)' : ''}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
-            return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
-        }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
-    },
-};
+        message.channel.send(`${StartEmbed}`);
+    }
+}
