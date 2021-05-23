be53e743c41cef1060726d755ec5a7791a788919
ItsJigzyy <albicus1994@gmail.com>
2021-05-23 03:27:13 +0100

    ff

--- commands/core/invite.js
+++ commands/core/invite.js
@@ -13,6 +13,7 @@ module.exports = {
             .setTitle("Click here to add")
             .setFooter("Thank you for using my bot - Jigzyy <3")
             .setURL("https://discord.com/api/oauth2/authorize?client_id=845396814688944148&permissions=8&scope=bot")
+            .attachFiles(['../assets/GrinningSmilePic.png'])
             .setThumbnail('attachment://GrinningSmilePic.png');
 
 
