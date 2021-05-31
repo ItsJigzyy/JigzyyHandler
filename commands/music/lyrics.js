c008f7d278627d3b8792cb73a0c754af8dc2f8e0
ItsJigzyy <albicus1994@gmail.com>
2021-05-31 09:20:50 +0100

    ttt

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -45,7 +45,7 @@ module.exports = {
                 });
                 console.log(query)
                 return query.join(' ')
-            } //else return arg.slice(1).join(' ');
+            } else return arg.slice(1).join(' ');
         };
 
 
