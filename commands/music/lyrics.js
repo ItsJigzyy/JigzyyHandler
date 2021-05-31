dd12bdafd700242a8e992bfbffb55d5804c7f80d
ItsJigzyy <albicus1994@gmail.com>
2021-05-31 18:42:09 +0100

    aaa

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -2,66 +2,61 @@ module.exports = {
     name: 'lyrics',
     aliases: ["ly"],
     category: 'Music',
-    utilisation: '{prefix}filter [filter name]',
+    utilisation: '{prefix}lyrics [Artist name]',
 
     execute(client, message, args) {
 
-        const getArtistTitle = require('get-artist-title');
-        const axios = require('axios');
-        const cheerio = require('cheerio');
+        const lyricsFinder = require("lyrics-finder");
         const { Client, MessageEmbed } = require("discord.js");
-        const baseURL = `https://api.genius.com/search?access_token=${client.config.discord.genius}`;
-        let playlist;
-
-        const scrapeLyrics = path => {
-            return axios.get(path)
-                .then(response => {
-                    let $ = cheerio.load(response.data);
-                    return [$('.header_with_cover_art-primary_info-title').text().trim(), $('.lyrics').text().trim()];
-                })
-                .catch(err => {
-                    console.warn(err);
-                });
-        };
-
-        const searchLyrics = url => {
-            return Promise.resolve(axios.get(url, { 'Authorization': `Bearer ${client.config.discord.genius}` })
-                .then(response => checkSpotify(response.data.response.hits))
-                .then(path => scrapeLyrics(path))
-                .catch(err => {
-                    console.warn(err);
-                })
-            );
-        };
-
-        const checkSpotify = hits => {
-            return hits[0].result.primary_artist.name === 'Spotify' ? hits[1].result.url : hits[0].result.url;
-        };
-
-        const createQuery = arg => {
-            if (arg === 'np') {
-                const query = [artist, title] = getArtistTitle(playlist.current, {
-                    defaultArtist: ' '
-                });
-                console.log(query)
-                return query.join(' ')
-            }// else return arg.slice(1).join(' ');
-        };
-
-
-        const query = createQuery(args[0]);
-        searchLyrics(`${baseURL}&q=${encodeURIComponent(query)}`)
-            .then(songData => {
-                const embed = new MessageEmbed()
-                    .setColor(0x00AE86)
-                    .setTitle(`Lyrics for: ${songData[0]}`)
-                    .setDescription(songData[1]);
-                return message.channel.send({ embed });
+        module.exports.run = async (client, message, args, searcher) => {
+        
+            if (args.length < 1) return message.reply("Please enter the Artist's name first. ~lyrics <Artist Name Here>")
+        
+            let artist = args.join(" ");
+            let songName = '';
+            let pages = [];
+            let currentPage = 0;
+        
+            const messageFilter = m => m.author.id === message.author.id;
+            const reactionFilter = (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && (message.author.id === user.id)
+        
+            message.channel.send("Please enter the song name now...");
+            await message.channel.awaitMessages(messageFilter, { max: 1, time: 15000 }).then(async collected => {
+                songName = collected.first().content;
+                await finder(artist, songName, message, pages)
             })
-            .catch(err => {
-                message.channel.send(`No lyrics found for: ${query} 🙁`, { code: 'asciidoc' });
-                console.warn(err);
-            });
+        
+            const lyricEmbed = await message.channel.send(`Lyrics page: ${currentPage + 1}/${pages.length}`, pages[currentPage])
+            await lyricEmbed.react('⬅️');
+            await lyricEmbed.react('➡️');
+        
+            const collector = lyricEmbed.createReactionCollector(reactionFilter);
+        
+            collector.on('collect', (reaction, user) => {
+                if (reaction.emoji.name === '➡️') {
+                    if (currentPage < pages.length - 1) {
+                        currentPage += 1;
+                        lyricEmbed.edit(`Lyrics page: ${currentPage + 1}/${pages.length}`, pages[currentPage]);
+                    }
+                } else if (reaction.emoji.name === '⬅️') {
+                    if (currentPage !== 0) {
+                        currentPage -= 1;
+                        lyricEmbed.edit(`Lyrics page: ${currentPage + 1}/${pages.length}`, pages[currentPage])
+                    }
+                }
+            })
+        }
+        
+        async function finder(artist, songName, message, pages){
+            let fullLyrics = await lyricsFinder(artist, songName) || "No results...";
+        
+            for (let i = 0; i < fullLyrics.length; i += 2048){
+                const lyric = fullLyrics.substring(i, Math.min(fullLyrics.length, i + 2048))
+                const msg = new MessageEmbed()
+                .setDescription(lyric)
+                pages.push(msg);
+            }
+        }
 
 
     }
