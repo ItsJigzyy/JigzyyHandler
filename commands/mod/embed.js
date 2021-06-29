938a5b2ae50005f9c778f4b9c011f1bbe51914b8
ItsJigzyy <albicus1994@gmail.com>
2021-06-29 18:16:21 +0100

    sss

--- commands/mod/embed.js
+++ commands/mod/embed.js
@@ -4,7 +4,7 @@ module.exports = {
     category: 'Mod',
     utilisation: '{prefix}embed',
 
-    execute(client, message, args) {
+    async execute(client, message, args) {
 
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`You don't have permission to use this command! \n Permission Required: **__MANAGE MESSAGES__**`)
         const sayMessage = args.join(" ");
