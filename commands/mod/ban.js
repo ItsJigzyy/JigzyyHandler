a47cb7234afc08eb738a86a4928f8c266f8c26a1
ItsJigzyy <albicus1994@gmail.com>
2021-06-29 15:32:55 +0100

    ee

---
+++ commands/mod/ban.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'Ban',
+    aliases: ['Ban'],
+    category: 'Mod',
+    utilisation: '{prefix}ban @user',
+
+    execute(client, message, args) {
+
+        message.channel.send("I work")
+
+    },
+};
