7562769711442af3fb23d5f828cba61c94f4acf6
ItsJigzyy <albicus1994@gmail.com>
2021-07-21 23:44:50 +0100

    fff

--- commands/infos/help.js
+++ commands/infos/help.js
@@ -1,7 +1,7 @@
 module.exports = {
-    name: 'help',
+    name: 'Help',
     aliases: ['commands'],
-    category: 'infos',
+    category: 'Infos',
     utilisation: '{prefix}help <command name>',
 
     execute(client, message, args) {
--- commands/infos/ping.js
+++ commands/infos/ping.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'ping',
+    name: 'Ping',
     aliases: [],
     category: 'Infos',
     utilisation: '{prefix}ping',
