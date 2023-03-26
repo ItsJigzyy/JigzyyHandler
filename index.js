c2f9a931d7ffe68ad50a7688fd73778fcf20b994
Albie <albicus1994@gmail.com>
2023-03-26 03:04:41 +0100

    gh

--- index.js
+++ index.js
@@ -64,12 +64,6 @@ for (const file of events) {
     client.on(file.split(".")[0], event.bind(null, client));
 };
 
-for (const file of player) {
-    console.log(`Loading discord-player event ${file}`);
-    const event = require(`./player/${file}`);
-    client.player.on(file.split(".")[0], event.bind(null, client));
-};
-
 //  Message Content Includes: 
 
 
